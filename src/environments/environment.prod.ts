b027eb85d8e02790f92cc903e8fe5bc8b8de1d44
Victor G <victorg590@gmail.com>
2020-02-15 16:14:31 +0100

    Add basic configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from './environment_interface';
+
+export const environment: Environment = {
+  production: true,
+  graphql_uri: '/graphql'
 };
